570c3707e5e82b7b2fe28687c7f3b5d2c800fc36
haileyringier <haileyringier@outlook.com>
2021-01-06 14:04:16 -0700

    adds function to check if snowflakes are unique

--- question1-1.js
+++ question1-1.js
@@ -31,3 +31,41 @@ function identical_right(snow1, snow2, start){
 
 // improving the function above using the mod operator
 
+function identical_right(snow1, snow2, start){
+    for(let offset = 0;offset < 6; offset++){
+        if(snow1[offset] != snow2[(start + offset) % 6]){
+            return 0 
+        }
+        return 1
+    }
+}
+
+// healper function for leftward check
+
+function identical_left(snow1, snow2, start){
+    let snow2_index = 0
+    for(let offset = 0; offset < 6; offset++){
+        snow2_index = start - offset
+        if(snow2_index < 0){
+            snow2_index = snow2_index + 6
+        }
+        if(snow1[offset] != snow2[snow2_index]){
+            return 0 
+        }
+    return 1
+    }
+}
+
+// are the snowflakes identical?
+
+function are_identical(snow1, snow2){
+    for(let start = 0; start < 6; start++){
+        if(identical_right(snow1, snow2, start)){
+            return 1
+        }
+        if(identical_left(snow1, snow2, start)){
+            return 1
+        }
+    }
+    return 0 
+}
